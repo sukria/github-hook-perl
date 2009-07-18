20d2240a863a937ad8f4eb0b3549229a619717a2
Alexis Sukrieh <sukria@sukria.net>
2009-07-18 17:18:03 +0200

    Revert "first Coat classes and moved github.cgi to bin"
    This reverts commit e76c76e5bfa1fca88a765601c9ef38a038b5e33c.

--- lib/GitHub/Hook/Config.pm
+++
@@ -1,7 +0,0 @@
-package GitHub::Hook::Config;
-use Coat;
-
-has smtp_host    => (isa => 'Str');
-has repositories => (isa => 'ArrayRef');
-
-'GitHub::Hook::Config';
--- lib/GitHub/Hook/Repository.pm
+++
@@ -1,8 +0,0 @@
-package GitHub::Hook::Repository;
-use Coat;
-
-has owner => (isa => 'Str');
-has url   => (isa => 'Str');
-has name  => (isa => 'Str');
-
-'GitHub::Hook::Repository';
